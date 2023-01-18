64657d2793cc222d06c54f30e66f5c821a6c63ea
SardorDev <sardorqobilov555@gmail.com>
2023-01-19 01:11:24 +0500

    amalitor formlar bilan

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,77 +1,67 @@
 import React from 'react';
+import { Form, Input, Badge } from 'reactstrap';
 
-class Form extends React.Component {
+class Forms extends React.Component {
   state = {
     firstName: '',
     email: '',
-    message: '',
-    select: '',
-    subscribe: false,
-    gender: ''
+    subscription: false,
   }
 
-  changeValue = (e) => {
+  handleChange = (e) => {
     this.setState({ [e.target.name]: e.target.value })
   }
 
-  changeCheck = (e) => {
-    this.setState({ [e.target.name]: e.target.checked })
-  }
-
-  validateName = () => {
-    if (this.state.firstName.length < 5) {
-      alert("Sizning ismingiz 5harifdan oshgan bo'lishi ketak")
-    }
+  checkHadle = (e) => {
+    this.setState({[e.target.name]: e.target.checked})
   }
   validateEmail = () => {
-    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
-    if (!re.test(this.state.email)) {
-      alert("Emailni notog'ri kiritingiz iltomos tekshirib qaytadan urinib ko'ring")
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const isValidEmail = re.test(this.state.email);
+    const isVaildChecked = this.state.subscription;
+
+    if (!isValidEmail) {
+      alert('Your email is not vaild');
+      return
     }
+    if (!isVaildChecked) {
+      alert('Your should accept all terms & conditions');
+      return
+    }
+
+    this.setState({ email: '', subscription: false})
+    alert('Thanks you for subscription')
   }
 
   render() {
-    const { firstName, email, message, select, subscribe, gender } = this.state
+    const { email, subscription } = this.state
     return (
-      <div className='posts'>
-        <h1>Form</h1>
-        <label htmlFor="">Your name </label>
-        <input type="text"
-          name='firstName'
-          value={firstName}
-          onChange={this.changeValue}
-          onBlur={this.validateName}
-        />
-        <br />
-        <label htmlFor="">Your email </label>
-        <input type="email"
-          name='email'
-          value={email}
-          onChange={this.changeValue}
-          onBlur={this.validateEmail}
-        /> <br />
-        <label htmlFor="">Meesage </label>
-        <textarea name="message"
-          placeholder='Message' 
-          value={message} 
-          onChange={this.changeValue}></textarea>
+      <div className='formApp'>
+        <h1 className='mb-4 text-center text-white'>Form</h1>
+          <label htmlFor="foo">Your email</label>
+          <Input
+            id='foo'
+            name="email"
+            placeholder="email"
+            type="email"
+            value={email}
+            onChange={this.handleChange}
+          />
+
+          <label htmlFor="">
+            <Input
+              type="checkbox"
+              name="subscription"
+              checked={subscription}
+              onChange={this.checkHadle}
+            />
+            Men barcha shartlarga roziman
+          </label>
           <br />
-        <select name="select" value={select} onChange={this.changeValue} id="">
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-          </select>
-          <br />
-          <label htmlFor="foo">
-            Subscribe
-          <input type="checkbox" id='foo' name="subscribe" value={subscribe} onChange={this.changeCheck} />
-          </label> <br />
-          <label htmlFor="">Gender</label>
-          <input type="radio" name="gender" value='male' onChange={this.changeValue} /> Male
-        <input type="radio" name="gender" value='female' onChange={this.changeValue} /> Female
+          <button className='btn btn-primary mt-3' onClick={this.validateEmail}>Primary</button>
       </div>
     );
   }
 }
 
-export default Form;
+export default Forms;
